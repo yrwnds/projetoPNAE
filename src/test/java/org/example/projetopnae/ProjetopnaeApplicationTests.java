e6e53c3b50b6c1a974953030622c52dee265f942
yrwnds <l.zanini.nunes@gmail.com>
2026-04-26 10:38:45 -0300

    first commit

---
+++ src/test/java/org/example/projetopnae/ProjetopnaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetopnae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopnaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
