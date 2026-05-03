1fda1bd90596601c05c25e7b9b067f01324f2d1b
yrwnds <l.zanini.nunes@gmail.com>
2026-05-03 10:55:40 -0300

    controller usuario

--- src/main/java/org/example/projetopnae/controller/UsuarioController.java
+++ src/main/java/org/example/projetopnae/controller/UsuarioController.java
@@ -2,10 +2,7 @@ package org.example.projetopnae.controller;
 
 import org.example.projetopnae.model.usuario.Usuario;
 import org.example.projetopnae.service.UsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,25 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public Usuario buscar(@PathVariable long id){
         return this.service.findById(id);
+    }
+
+    @PostMapping("/print-json")
+    public void printJson(@RequestBody String json){
+        System.out.println(json);
+    }
+
+    @PostMapping()
+    public void salvar(@RequestBody Usuario usuario){
+        this.service.save(usuario);
+    }
+
+    @PutMapping
+    public void editar(@RequestBody Usuario usuario){
+        this.service.update(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable long id){
+        this.service.delete(id);
     }
 }
